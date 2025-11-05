472283ad587267e203a0a1341bff2cb16c4d3fbb
Galym Kumiskhan <galymkumiskhan@MacBook-Air-Galym.local>
2025-11-06 00:34:09 +0500

    Add Render domain to ALLOWED_HOSTS

--- cards_aster/settings.py
+++ cards_aster/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,7 @@ WSGI_APPLICATION = 'cards_aster.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
